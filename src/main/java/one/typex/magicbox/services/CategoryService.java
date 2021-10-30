b8609eab86fd52ea85ee2d47f7e3588b35e96dd0
xinik <sergey20160@gmail.com>
2021-10-30 22:24:54 +0300

    Change byte convertor

--- src/main/java/one/typex/magicbox/services/CategoryService.java
+++ src/main/java/one/typex/magicbox/services/CategoryService.java
@@ -11,8 +11,6 @@ import java.util.List;
 @Service
 public class CategoryService {
 
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-
     private final CategoryRepo categoryRepo;
 
     public CategoryService(CategoryRepo categoryRepo) {
@@ -25,27 +23,17 @@ public class CategoryService {
                         new CategoriesDto(
                                 it.getId(),
                                 it.getName(),
-                                getColorHexString(it.getColor())
+                                new String(it.getColor(), StandardCharsets.UTF_8)
                         )
                 ).toList();
     }
 
-    private String getColorHexString(byte[] colorByte) {
-        char[] hexChars = new char[colorByte.length * 2];
-        for (int j = 0; j < colorByte.length; j++) {
-            int v = colorByte[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
 
     public void addCategories(List<CategoriesDto> categoriesDto) {
-        for (var categoryDto: categoriesDto) {
+        for (var categoryDto : categoriesDto) {
             var category = new Category();
             category.setName(categoryDto.getName());
             var color = categoryDto.getColor();
-            color = color.substring(1);
             category.setColor(color.getBytes(StandardCharsets.UTF_8));
             categoryRepo.save(category);
         }
