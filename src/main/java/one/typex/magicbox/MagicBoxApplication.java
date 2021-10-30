bd8a32ca305d7cfc0932f47755fd7ef41091d08f
Kirill Nikolaev <kn@typex.one>
2021-10-30 12:45:50 +0300

    Init

---
+++ src/main/java/one/typex/magicbox/MagicBoxApplication.java
@@ -0,0 +1,13 @@
+package one.typex.magicbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicBoxApplication.class, args);
+    }
+
+}
